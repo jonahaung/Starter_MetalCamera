2b1d8b564cf882dc1a6efff129a5c8e25ef3a573
Aung Ko Min <jonahaung@gmail.com>
2020-04-18 11:07:13 +0800

    Initial Commit

---
+++ Starter_MetalCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Starter_MetalCamera
+//
+//  Created by Aung Ko Min on 18/4/20.
+//  Copyright © 2020 Aung Ko Min. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
